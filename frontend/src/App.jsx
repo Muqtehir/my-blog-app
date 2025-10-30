0a4a6f6b395c8817cbcfa176dcde1091d3131ae4
Muqtehir Mayaleeke <Izzatudeen Muqtehir>
2025-10-30 21:15:11 +0100

    feat(frontend): add 'Create a Blog Website' content to App and styles

---
+++ frontend/src/App.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import "./CreateBlog.css";
+
+function App() {
+  return (
+    <main className="create-blog">
+      <div className="create-blog__inner">
+        <h2 className="muted">CREATE A BLOG WEBSITE</h2>
+        <h1 className="title">CREATE A BLOG WEBSITE</h1>
+
+        <p className="lead">
+          A blog website is one where users can put up their thoughts about
+          anything and everything. You can make it as creative as you want to.
+          There are thousands of blog websites out there. You can take
+          inspiration from any of these blog websites and create your own. Here,
+          people have to log in using their credentials and proper
+          authentication will also be done. After that, the user can put up
+          their blogs for the public and they should be stored in the database
+          so that it remains there. Also, the functionality of editing a blog
+          after posting it and deleting it from the post would be a cherry on
+          the cake.
+        </p>
+
+        <h3 className="section-title">
+          The knowledge that you will gain from this project:
+        </h3>
+
+        <p>
+          The front-end of a blog website should be really impressive for the
+          bloggers to be attracted to your website. So, a lot of hard work and
+          learning will go into the front end. Also, all the blogs will be
+          stored in the database and if the blogger edits it, changes should be
+          reflected in the database too. So, handling the database and making an
+          attractive front-end are the main learnings that one can get from this
+          project. The GitHub Link for a sample Blog Website with its source
+          code is given below. You can take inspiration from it and try to make
+          your own attractive and amazing blog website.
+        </p>
+      </div>
+    </main>
+  );
+}
+
+export default App;
